763f21f0dacf2e4d7e631f2c5006b15b2fc90b54
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-07 14:42:41 -0800

    add .env file, install new plugin to use .env, add plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const Dotenv = require('dotenv-webpack'); 
 const path = require('path');
 
 module.exports = {
@@ -18,7 +19,8 @@ module.exports = {
       title: 'haiku',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
